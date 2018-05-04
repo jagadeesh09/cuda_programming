ea9abd7f374fc7f213065231b09d847a676559f8
Jagadeesh Dondeti <jagadeesh@uncannyvision.com>
2018-05-04 06:57:41 +0000

    Addition of vector is shown

--- example.cu
+++ example.cu
@@ -1,41 +1,65 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <time.h>
+
+#define N 512
 
 
-__global__ void mykernel(void) {
-}
 
 __global__ void add(int *a, int *b, int *c){
 
-    *c = *a + *b;
+    c[blockIdx.x] = a[blockIdx.x] + b[blockIdx.x];
 
 }
 
+void random_ints(int *a, int n)
+{
+
+    int i;
+    for (i=0; i < n; ++i){
+
+        a[i] = rand();
+
+    }
+
+}
 int main(void) {
     
-    mykernel<<<1,1>>>();
-    printf("Hello World\n");
-
-    int a, b, c;
+    int *a, *b, *c;
     int *d_a, *d_b, *d_c;
-    int size = sizeof(int);
+    int size = N * sizeof(int);
 
     cudaMalloc((void **)&d_a, size);
     cudaMalloc((void **)&d_b, size);
     cudaMalloc((void **)&d_c, size);
 
-    a = 2;
-    b = 5;
+    a = (int *)malloc(size);
+    b = (int *)malloc(size);
+    c = (int *)malloc(size);
+
+    random_ints(b, N);
+    random_ints(a, N);
 
     cudaMemcpy(d_a, &a, size, cudaMemcpyHostToDevice);
     cudaMemcpy(d_b, &b, size, cudaMemcpyHostToDevice);
+    
+    clock_t t;
+    t = clock();
 
-    add<<<1,1>>>(d_a, d_b, d_c);
+    add<<<N,1>>>(d_a, d_b, d_c);
 
-    cudaMemcpy(&c, d_c, size, cudaMemcpyDeviceToHost);
+    t = clock() - t;
+    double time_taken = ((double)t)/CLOCKS_PER_SEC;
+    printf("Time taken by function is %f seconds\n",time_taken);
+
+    cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
 
     cudaFree(d_a);
     cudaFree(d_c);
     cudaFree(d_c);
+
+    free(a);
+    free(b);
+    free(c);
     return 0;
 
 }
